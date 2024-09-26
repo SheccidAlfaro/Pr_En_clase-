13ec62292a1fced4d222bd8509cb4485545e90b0
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-25 20:22:19 -0600

    Add files via upload

---
+++ Pr_en_clase#4/Pr_en_clase#4.py
@@ -0,0 +1,10 @@
+print("")
+print("Sacar el promedio de tres numeros Alfaro Ibarra Sheccid 3W")
+#Declarar variables y decirle al usuario que incerte tres numeros
+num1=float(input("Incerta el primer numero:"))
+num2=float(input("Inserta el segundo numero"))
+num3=float(input("Inserta el tercer numero:"))
+#define varible para llevar a cabo la formula
+promedio=num1+num2+num3/3
+print("El promedio es:")#mostrar resultado en pantalla
+print(promedio)
