9f9eb27bba22958516da4ab374e942fc8367c43d
SheccidAlfaro <a.23308051281171@cbtis128.edu.mx>
2024-09-25 19:57:04 -0600

    Add files via upload

---
+++ pr_en_clase_/main.py
@@ -0,0 +1,10 @@
+print("")
+print("Alfaro Ibarra Sheccid 3W, multiplicacion de tres numeros.")
+numero1=float(input("Pon el primer numero:"))
+numero2=float(input("Pon el segundo numero:"))
+numero3=float(input("Pon el tercer numero:"))
+
+mult=numero1*numero2*numero3
+print("el resultado es:")
+print(mult)
+print("")
